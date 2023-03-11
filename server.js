d4cec52dd08ce5b262c5a2eafab04e2d1ecdcc84
excelzed <excelzed@gmail.com>
2023-03-11 16:15:05 +0100

    first commit

--- server.js
+++ server.js
@@ -317,7 +317,7 @@ app.get('/src/public/Images/:filename/:filename', (req, res) => {
 // }
 // );
 // Connect to MongoDBconst
-mongoose.connect(`mongodb://mongo:ppBUD8hmf6puVdcDX5FQ@containers-us-west-28.railway.app:5954`);
+mongoose.connect(`mongodb://mongo:${process.env.mongopassword}@containers-us-west-28.railway.app:5954`);
 const db = mongoose.connection;
 const { GridFSBucket } = require('mongodb');
   // Create a new GridFSBucket object
